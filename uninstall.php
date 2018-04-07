49b9e1dba708f124c654d8701e7606b6d878e8c6
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2018-04-07 18:38:59 +0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,3 +2,25 @@
 defined('WP_UNINSTALL_PLUGIN') || die();
 
 delete_option('wwhwl_slug');
+
+if (is_multisite()) {
+	$network = get_network();
+	if ($network) {
+		$offset = 0;
+		$number = 100;
+
+		do {
+			$sites = get_sites(['network_id' => $network->id, 'offset' => $offset, 'number' => $number, 'fields' => 'ids']);
+
+			foreach ($sites as $site_id) {
+				switch_to_blog($site_id);
+				delete_option('wwhwl_slug');
+			}
+
+			restore_current_blog();
+			$offset += count($sites);
+		} while (count($sites) > 0);
+
+		delete_site_option('wwhwl_slug');
+	}
+}
